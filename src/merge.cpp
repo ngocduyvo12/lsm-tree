2106ed7c484375e0efb14723e890d1925e123dc3
Ngoc Vo <ngocduyvo12@gmail.com>
2023-05-07 19:42:04 -0600

    added more inline comments

--- src/merge.cpp
+++ src/merge.cpp
@@ -3,37 +3,42 @@
 
 #include "merge.h"
 
+// The add function adds a batch of entries (a run) to the MergeContext.
 void MergeContext::add(entry_t *entries, long num_entries) {
     merge_entry_t merge_entry;
 
-    if (num_entries > 0) {
-        merge_entry.entries = entries;
-        merge_entry.num_entries = num_entries;
-        merge_entry.precedence = queue.size();
-        queue.push(merge_entry);
+    if (num_entries > 0) { // Check if there are any entries to add
+        merge_entry.entries = entries; // Set the entries pointer to the given entries
+        merge_entry.num_entries = num_entries; // Set the number of entries
+        merge_entry.precedence = queue.size(); // Set the precedence based on the current queue size
+        queue.push(merge_entry); // Add the merge_entry to the priority queue
     }
 }
 
+// The next function returns the next entry to be merged based on the merge_entry_t structures in the priority queue.
 entry_t MergeContext::next(void) {
     merge_entry_t current, next;
     entry_t entry;
 
-    current = queue.top();
+    current = queue.top(); // Get the top element from the priority queue
     next = current;
 
-    // Only release the most recent value for a given key
+    // While loop ensures that only the most recent value for a given key is returned
     while (next.head().key == current.head().key && !queue.empty()) {
-        queue.pop();
+        queue.pop(); // Remove the top element from the priority queue
 
-        next.current_index++;
-        if (!next.done()) queue.push(next);
+        next.current_index++; // Increment the current_index of the next merge_entry_t
+        if (!next.done()) { // Check if there are more entries in the next merge_entry_t
+            queue.push(next); // Push the updated merge_entry_t back into the priority queue
+        }
 
-        next = queue.top();
+        next = queue.top(); // Update the next merge_entry_t with the new top element from the priority queue
     }
 
-    return current.head();
+    return current.head(); // Return the most recent value for the given key
 }
 
+// The done function checks if all entries have been merged and returns true if the priority queue is empty.
 bool MergeContext::done(void) {
     return queue.empty();
 }
--- src/merge.h
+++ src/merge.h
@@ -5,30 +5,45 @@
 
 using namespace std;
 
+// Define the merge_entry structure which holds information about a run of entries
 struct merge_entry {
-    int precedence;
-    entry_t *entries;
-    long num_entries;
-    int current_index = 0;
+    int precedence; // Precedence value to break ties when keys are equal
+    entry_t *entries; // Pointer to the array of entries in the run
+    long num_entries; // The number of entries in the run
+    int current_index = 0; // The current index of the entry being processed in the run
+
+    // Return the entry at the current_index
     entry_t head(void) const {return entries[current_index];}
+
+    // Return true if the current_index is equal to the number of entries in the run, indicating that the run has been processed
     bool done(void) const {return current_index == num_entries;}
+
+    // Overload the '>' operator to compare merge_entry objects
     bool operator>(const merge_entry& other) const {
         // Order first by keys, then by precedence
-        if (head() == other.head()) {
+        if (head() == other.head()) { // If the keys are equal, compare based on precedence
             assert(precedence != other.precedence);
             return precedence > other.precedence;
-        } else {
+        } else { // Otherwise, compare based on the keys
             return head() > other.head();
         }
     }
 };
 
+// Typedef for easier readability
 typedef struct merge_entry merge_entry_t;
 
+// Define the MergeContext class which handles merging of runs
 class MergeContext {
+    // Declare a priority queue to store merge_entry_t objects, sorted by keys and precedence
     priority_queue<merge_entry_t, vector<merge_entry_t>, greater<merge_entry_t>> queue;
 public:
+    // Add a run of entries to the MergeContext
     void add(entry_t *, long);
+
+    // Get the next entry to be merged
     entry_t next(void);
+
+    // Check if all runs have been processed
     bool done(void);
 };
